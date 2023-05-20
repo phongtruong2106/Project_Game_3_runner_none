0915cff57f721bba1819d8ccac67cf7344f958b3
phongtruong2106 <Thpgamer42@gmail.com>
2023-05-20 23:02:39 +0700

    up Generation Of Platform

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -17,6 +17,7 @@ public class Player : MonoBehaviour
 
    public bool isHoldingJump = false;
    public float maxHoldJumpTime = 0.4f;
+   public float maxMaxHoldJumpTime = 0.4f;
    public float holdJumpTimer = 0.0f;
 
    public float jumpGroundThreshold = 1f;
@@ -98,6 +99,8 @@ public class Player : MonoBehaviour
             float velocityRatio = velocity.x / maxVelocity;
            
             acceleration = maxAcceleration * (1 - velocityRatio);
+            maxHoldJumpTime = maxMaxHoldJumpTime * velocityRatio;
+
             velocity.x += acceleration * Time.fixedDeltaTime;
             if(velocity.x >= maxVelocity)
             {
